50a1d1cd4b72b0c31249469d468bc976f34d9ab5
aleksmaksimovic <aleksjohannes.maksimovic@gmail.com>
2016-05-18 19:37:01 +0200

    maksimovic_sales - This is a push.

---
+++ Maksimovic_SALES/src/domain/Customer.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Customer
+{
+	private String firstName;
+	public String getFirstName()
+	
+	
+	
+	{
+		return firstName;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public double getCredit()
+	{
+		return credit;
+	}
+	public void setCredit(double credit)
+	{
+		this.credit = credit;
+	}
+	private double credit;
+	
+	
+
+}
---
+++ Maksimovic_SALES/src/domain/Customers.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Customers
+{
+	private ArrayList<Customer>customers;
+	
+	
+
+	public ArrayList<Customer> getCustomers()
+	{
+		return customers;
+	}
+
+	public void setCustomers(ArrayList<Customer> customers)
+	{
+		this.customers = customers;
+	}
+
+	
+	
+}
